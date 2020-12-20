39469b9074b51c7d173926fa8d4ec69956b50038
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2020-12-20 20:54:39 +0300

    доделал main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,10 @@
 import './Main.css';
+import SearchContainer from '../SearchContainer/SearchContainer';
 
 function Main() {
     return (
       <div className="main">
-        <h1 className="main__header-text">Что творится в мире?</h1>
-        <p className="main__header-subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
-        <div>
-          <input></input>
-          <button className="main__search-for-news"></button>
-        </div>
+        <SearchContainer/>
       </div>
     );
   }
