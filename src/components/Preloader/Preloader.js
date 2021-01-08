fe22a5fd08168cc5e3b6b0171e86fe998cde8ffd
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2021-01-08 18:55:28 +0300

    дополнение Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,12 +1,22 @@
 import './Preloader.css';
 import preloader from '../../images/preloader.png';
+import notFound from '../../images/not-found.png';
 
 function Preloader() {
     return (
+      <>
       <div className="preloader-container">
         <img className="preloader-container__preloader" src={preloader} alt="прелоудер"/>
-        <div className="preloader-container__text-inform">Идет поиск новостей...</div>
+        <p className="preloader-container__text-inform">Идет поиск новостей...</p>
       </div>
+      <div className="preloader-container">
+        <div className="preloader-container__not-found">
+          <img className="preloader-container__not-found-image" src={notFound} alt="Ничего не найдено"/>
+          <h2 className="preloader-container__text-header">Ничего не найдено</h2>
+          <p className="preloader-container__not-found-text-inform">К сожалению по вашему запросу ничего не найдено.</p>
+        </div>
+      </div>
+      </>
     );
   }
   
