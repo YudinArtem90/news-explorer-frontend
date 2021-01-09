96898680342ce8eb8c5890738b420ec2aa62a964
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2021-01-09 20:57:48 +0300

    адаптив для search-form

--- src/components/SearchContainer/SearchContainer.js
+++ src/components/SearchContainer/SearchContainer.js
@@ -4,8 +4,10 @@ import SearchForm from '../SearchForm/SearchForm';
 function SearchContainer() {
     return (
         <div className="search-container">
-            <h1 className="search-container__header-text">Что творится в мире?</h1>
-            <p className="search-container__header-subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+            <div className="search-container__info-text">
+                <h1 className="search-container__header-text">Что творится в мире?</h1>
+                <p className="search-container__header-subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+            </div>
             <SearchForm/>
         </div>
     );
