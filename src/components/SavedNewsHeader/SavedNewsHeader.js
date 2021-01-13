2005dda382333fe65bfcc2872fcfb5cb894e826c
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2021-01-13 14:55:16 +0300

    адаптив для страницы сохраненых карточек + сама страница

---
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -0,0 +1,20 @@
+import './SavedNewsHeader.css';
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom'; 
+import NewsCardList from '../NewsCardList/NewsCardList';
+
+function SavedNewsHeader(props) {
+
+    return (
+      <>
+        <div className='saved-news-header'>
+          <p className='saved-news-header__save-news'>Сохранённые статьи</p>
+          <h2 className='saved-news-header__header-text'>Грета, у вас 5 сохранённых статей</h2>
+          <p className='saved-news-header__keywords'>По ключевым словам: <strong>Природа</strong>, <strong>Тайга</strong> и <strong>2-м другим</strong></p>
+        </div>
+        <NewsCardList/>
+      </>
+    );
+  }
+  
+export default withRouter(SavedNewsHeader);
