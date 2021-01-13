23183f5c0a0ee58e02ea16b7f2a5a5b9817c3ac3
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2021-01-13 14:54:47 +0300

    адаптив для листа карточек

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,10 +1,18 @@
 import './NewsCardList.css';
 import Card from '../Card/Card';
+import { Link, withRouter } from 'react-router-dom';
 
-function NewsCardList() {
+function NewsCardList(props) {
+
+    const mainPage = props.location.pathname === '/';
+
     return (
-        <div className="news-card-list-container">
-            <h2 className="news-card-list-container__title">Результаты поиска</h2>
+        <div className={`news-card-list-container ${mainPage ? 'news-card-list-container_theme_main-page' : 'news-card-list-container_theme_saved-news'}`}>
+
+            {
+                mainPage ?  <h2 className="news-card-list-container__title">Результаты поиска</h2> : null
+            }
+            
             <div className="news-card-list-container__card-list">
             <>
                 <Card/>
@@ -43,6 +51,7 @@ function NewsCardList() {
             <>
                 <div className="new-card-container">
                     <div className="new-card-container__header">
+                        <label className="new-card-container__inform-label">Убрать из сохранённых</label>
                         <button className="new-card-container__button new-card-container__button_delete-articles"></button>
                     </div>
                     <div className="new-card-container__main">
@@ -58,11 +67,12 @@ function NewsCardList() {
             <>
                 <div className="new-card-container">
                     <div className="new-card-container__header">
+                        <label className="new-card-container__inform-label">Убрать из сохранённых</label>
                         <button className="new-card-container__button new-card-container__button_save-articles"></button>
                     </div>
                     <div className="new-card-container__main">
                         <label className="new-card-container__date">2 августа, 2019</label>
-                        <h2 className="new-card-container__title">Национальное достояние – парки</h2>
+                        <h2 className="new-card-container__title">«Первозданная тайга»: новый фотопроект Игоря Шпиленка</h2>
                         <p className="new-card-container__article">В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков – охраняемых территорий, где и сегодня каждый может приобщиться к природе.</p>
                         <label className="new-card-container__source-of-information">Лента.ру</label>
                     </div>
@@ -86,9 +96,13 @@ function NewsCardList() {
             </>
 
             </div>
-            <button className="news-card-list-container__button">Показать еще</button>
+
+            {
+                mainPage ?  <button className="news-card-list-container__button">Показать еще</button> : null
+            }
+            
         </div>
     );
   }
   
-  export default NewsCardList;
+  export default withRouter(NewsCardList);
