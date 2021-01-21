8c9b33eb99687ece82957a8f3f98f10061b31d62
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2021-01-21 14:08:15 +0300

    подправил порт бэка

--- backend/helpers/constants/index.js
+++ backend/helpers/constants/index.js
@@ -3,7 +3,7 @@ const regExpUrl = '^(http|https):\/\/([A-Za-z]|\.|[0-9]|\/|\#|\-|\_)+';
 const regExpObjectId = '^[0-9a-fA-F]{24}$';
 // eslint-disable-next-line no-useless-escape
 const regExpEmail = '^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$';
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 module.exports = {
   regExpUrl,
